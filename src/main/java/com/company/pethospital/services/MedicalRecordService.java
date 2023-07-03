aff4876e53a92ece44ee82447862649bd4bb5386
Maxnetz <tanapongt9496@gmail.com>
2023-07-03 21:46:43 +0700

    feat: update MedicalRecord Services

--- src/main/java/com/company/pethospital/services/MedicalRecordService.java
+++ src/main/java/com/company/pethospital/services/MedicalRecordService.java
@@ -1,4 +1,16 @@
 package com.company.pethospital.services;
 
+import com.company.pethospital.models.Billing;
+import com.company.pethospital.models.MedicalRecord;
+
+import java.util.List;
+
 public interface MedicalRecordService {
+    List<MedicalRecord> findAllRecords();
+
+    MedicalRecord findRecordById(Long id);
+
+    MedicalRecord createRecord(MedicalRecord medicalRecord);
+
+    void deleteRecord(Long id);
 }
